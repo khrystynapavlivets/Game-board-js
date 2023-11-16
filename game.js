70f4f8e12b9e7d680e53dbb8bbcef1f03c8aaaba
khrystynapavlivets <khristina.pavlivets@gmail.com>
2023-11-16 11:54:49 +0200

    changed function removeGroupRecursive

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
-// Функція removeGroupRecursive видаляє усю повʼязану групу таких самих елементів
-
+// Функція removeGroupRecursive видаляє всі елементи з однаковим значенням вказаної комірки та її сусідів, якщо вони також мають однакове значення.
 class Cell {
     constructor(value, row, col) {
         this.value = value;
@@ -27,32 +26,34 @@ class GameBoard {
         return values[randomIndex];
     }
 
-    removeGroupRecursive(row, col, targetValue, visited) {
-
-        visited[row][col] = true;
-        const currentCell = this.cells[row][col];
-
-        if (currentCell && currentCell.value === targetValue) {
-            this.cells[row][col] = null;
-
-            this.removeGroupRecursive(row + 1, col, targetValue, visited);
-            this.removeGroupRecursive(row - 1, col, targetValue, visited);
-            this.removeGroupRecursive(row, col + 1, targetValue, visited);
-            this.removeGroupRecursive(row, col - 1, targetValue, visited);
-        }
-    }
-
     removeGroup(row, col) {
         const selectedCell = this.cells[row][col];
-
         const targetValue = selectedCell.value;
         const visited = Array.from({ length: this.rows }, () => Array(this.cols).fill(false));
 
-        this.removeGroupRecursive(row, col, targetValue, visited);
-        this.display();
+        const removeGroupRecursive = (row, col) => {
+            if (row < 0 || row >= this.rows || col < 0 || col >= this.cols || visited[row][col]) {
+                return;
+            }
+
+            visited[row][col] = true;
+            const currentCell = this.cells[row][col];
+
+            if (currentCell != null && currentCell.value === targetValue) {
+                this.cells[row][col] = null;
+
+                removeGroupRecursive(row + 1, col);
+                removeGroupRecursive(row - 1, col);
+                removeGroupRecursive(row, col + 1);
+                removeGroupRecursive(row, col - 1);
+            }
+        };
+
+        removeGroupRecursive(row, col);
+        this.showBoard();
     }
 
-    display() {
+    showBoard() {
         const gameContainer = document.getElementById('game-container');
         gameContainer.innerHTML = '';
 
@@ -70,10 +71,8 @@ class GameBoard {
         }
     }
 }
-
 const gameBoard = new GameBoard(7, 6);
-gameBoard.display();
-
+gameBoard.showBoard();
 
 
 // Функція removeGroupRecursive проходить по всім значеннях вказаному діапазоні
@@ -109,47 +108,41 @@ gameBoard.display();
 //         const selectedCell = this.cells[row][col];
 //         const targetValue = selectedCell.value;
 //         const visited = Array.from({ length: this.rows }, () => Array(this.cols).fill(false));
-
+        
 //         const removeGroupRecursive = (row, col) => {
 //             if (row < 0 || row >= this.rows || col < 0 || col >= this.cols || visited[row][col]) {
 //                 return;
 //             }
-
 //             visited[row][col] = true;
 //             const currentCell = this.cells[row][col];
-
-
-
-//             if (currentCell && currentCell.value === targetValue) {
+//             if (currentCell != null  && currentCell.value === targetValue) {
 //                 this.cells[row][col] = null;
-
 //                 for (let i = -7; i <= 7; i++) {
-//                     removeGroupRecursive(row + i, col);
+//                     if (this.cells[row + i] && this.cells[row + i][col] && this.cells[row + i][col].value === targetValue) {
+//                         removeGroupRecursive(row + i, col);
+//                     }
 //                 }
-
 //                 for (let i = -6; i <= 6; i++) {
-//                     removeGroupRecursive(row, col + i);
-//                 }
+//                     if (this.cells[row][col + i] && this.cells[row][col + i].value === targetValue) {
+//                         removeGroupRecursive(row, col + i);
+//                     }
+//                 }    
 //             }
 //         };
-
 //         removeGroupRecursive(row, col);
-//         this.display();
+//         this.showBoard();
 //     }
 
-//     display() {
+//     showBoard() {
 //         const gameContainer = document.getElementById('game-container');
 //         gameContainer.innerHTML = '';
-
 //         for (let row = 0; row < this.rows; row++) {
 //             for (let col = 0; col < this.cols; col++) {
 //                 const cell = this.cells[row][col];
-
 //                 const cellElement = document.createElement('div');
 //                 cellElement.className = 'game-cell';
 //                 cellElement.textContent = cell ? cell.value : '';
 //                 cellElement.addEventListener('click', () => this.removeGroup(row, col));
-
 //                 gameContainer.appendChild(cellElement);
 //             }
 //         }
@@ -157,6 +150,4 @@ gameBoard.display();
 // }
 
 // const gameBoard = new GameBoard(7, 6);
-// gameBoard.display();
-
-
+// gameBoard.showBoard();
